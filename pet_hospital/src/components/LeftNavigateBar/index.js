acf2cca11cef0666390aba3f327c18d4e3c633e1
buhezhenqi <1540780810@qq.com>
2024-03-07 13:09:29 +0800

    提交main文件

--- pet_hospital/src/components/LeftNavigateBar/index.js
+++ pet_hospital/src/components/LeftNavigateBar/index.js
@@ -1,43 +1,36 @@
 import React from 'react'
 import './index.css'
-export default function
-    () {
+export default function LeftNavigateBar() {
     return (
-        <div class='navbar'>
-            <input type='checkbox' id='checkbox'></input>
-            <label for='checkbox'>
-                <i class="fa-solid fa-bars"></i>
-            </label>
-            <ul>
-                <li>
-                    <img src="./1.ico" alt=""></img>
-                    <span>欢迎您</span>
-                </li>
-                <li>
-                    <a href='javascript:void(0)'>
-                        <i class="fa-solid fa-user"></i>
-                        <span>用户管理</span>
-                    </a>
-                </li>
-                <li>
-                    <a href='javascript:void(0)'>
-                        <i class="fa-solid fa-briefcase-medical"></i>
-                        <span>病例管理</span>
-                    </a>
-                </li>
-                <li>
-                    <a href='javascript:void(0)'>
-                        <i class="fa-regular fa-file-lines"></i>
-                        <span>题库管理</span>
-                    </a>
-                </li>
-                <li>
-                    <a href='javascript:void(0)'>
-                        <i class="fa-solid fa-scroll"></i>
-                        <span>试卷管理</span>
-                    </a>
-                </li>
-            </ul>
-        </div >
+        <ul>
+            <li>
+                <img src="./1.ico" alt=""></img>
+                <span>欢迎您</span>
+            </li>
+            <li>
+                <a href='javascript:void(0)'>
+                    <i class="fa-solid fa-user"></i>
+                    <span>用户管理</span>
+                </a>
+            </li>
+            <li>
+                <a href='javascript:void(0)'>
+                    <i class="fa-solid fa-briefcase-medical"></i>
+                    <span>病例管理</span>
+                </a>
+            </li>
+            <li>
+                <a href='javascript:void(0)'>
+                    <i class="fa-regular fa-file-lines"></i>
+                    <span>题库管理</span>
+                </a>
+            </li>
+            <li>
+                <a href='javascript:void(0)'>
+                    <i class="fa-solid fa-scroll"></i>
+                    <span>试卷管理</span>
+                </a>
+            </li>
+        </ul>
     )
 }
