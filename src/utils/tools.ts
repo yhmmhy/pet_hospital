6c233f9fec301139d56fc44ebfed3f3e4d94a7dc
yuyuc <731358766@qq.com>
2024-03-29 17:17:43 +0800

    yjamerge

--- src/utils/tools.ts
+++
@@ -1,5 +0,0 @@
-export const serverUrl ='http://localhost:3007';
-
-export const setToken =(token:string)=> sessionStorage.setItem('token',token)
-
-export const getToken = ()=> sessionStorage.getItem('token')
