0ebd54c1ef7f07caa6175e55c753c80a381cf546
Hm <1796137708@qq.com>
2024-04-10 16:14:44 +0800

    delete

--- nodejs/router/Home.js
+++
@@ -1,5 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const home_handler = require('../router_handler/Home')
-router.get('/Statistics', home_handler.Statistics)
-module.exports = router
--- nodejs/router/charge.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const charge_handler = require('../router_handler/charge')
-router.get('/ProjectListGet', charge_handler.ProcjectListGet)
-router.post('/ProjectAdd',charge_handler.ProjectAdd)
-router.post('/ProjectModify',charge_handler.ProjectModify)
-router.post('/ProjectDelete',charge_handler.ProjectDelete)
-module.exports = router
